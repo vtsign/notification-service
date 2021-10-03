6684213c7b1557f9a18b1a89fffd3c0b18e08ebc
ngthotuan <ngthotuan@gmail.com>
2021-10-02 21:13:06 -0700

    update consumer

--- src/main/java/tech/vtsign/notificationservice/config/KafkaConsumerConfig.java
+++ src/main/java/tech/vtsign/notificationservice/config/KafkaConsumerConfig.java
@@ -1,15 +1,14 @@
 package tech.vtsign.notificationservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import tech.vtsign.notificationservice.model.Receiver;
 import tech.vtsign.notificationservice.model.User;
 
 import java.util.HashMap;
@@ -52,50 +51,69 @@ import java.util.Map;
 //        return new Receiver();
 //    }
 //}
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-import java.util.HashMap;
-import java.util.Map;
+//@Configuration
+//public class KafkaConsumerConfig {
+//    @Bean
+//    public Map<String, Object> consumerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "vtsign.tech:9092");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "tech.vtsign.notificationservice.model.*");
+//        return props;
+//    }
+//
+//
+//    @Bean
+//    public ConsumerFactory<String, User> consumerFactory() {
+//
+////        var deserializer = new JsonDeserializer<User>();
+////        deserializer.addTrustedPackages("tech.vtsign.notificationservice.model.*");
+//        return new DefaultKafkaConsumerFactory<>(
+//                consumerConfigs(),
+//                new StringDeserializer(),
+//                new JsonDeserializer<User>());
+//    }
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, User> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+//
+//}
 
 @Configuration
+@EnableKafka
 public class KafkaConsumerConfig {
+
     @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "vtsign.tech:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "tech.vtsign.notificationservice.model.*");
-        return props;
+    public ConsumerFactory<String, User> consumerFactory(){
+        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
+        Map<String, Object> configs = new HashMap<>();
+
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "vtsign.tech:9092");
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+
+        return new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), deserializer);
     }
 
-
     @Bean
-    public ConsumerFactory<String, User> consumerFactory() {
-
-//        var deserializer = new JsonDeserializer<User>();
-//        deserializer.addTrustedPackages("tech.vtsign.notificationservice.model.*");
-        return new DefaultKafkaConsumerFactory<>(
-                consumerConfigs(),
-                new StringDeserializer(),
-                new JsonDeserializer<User>());
-    }
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, User> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
-    }
 
+    }
 }
--- src/main/java/tech/vtsign/notificationservice/service/Consumer.java
+++ src/main/java/tech/vtsign/notificationservice/service/Consumer.java
@@ -1,20 +1,24 @@
 package tech.vtsign.notificationservice.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import tech.vtsign.notificationservice.model.User;
 
 import java.io.IOException;
 
+
 @Service
 @Slf4j
 public class Consumer {
 
-    @KafkaListener(topics = "user_test", groupId = "group_id")
-    public void consume(User user) throws IOException {
-        log.info(String.format("#### -> Consumed message -> %s", user));
+
+    @KafkaListener(topics = "final-topic", groupId = "group_one", containerFactory = "kafkaListenerContainerFactory")
+    public void consumeUserMessage(@Payload User user, @Headers MessageHeaders headers) throws IOException {
+        log.info("received data in Consumer="+ user);
+
     }
 }
