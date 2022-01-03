b72ac316b6e737dc6add825a77e6015f1385e439
ngthotuan <ngthotuan@gmail.com>
2022-01-03 22:54:49 +0700

    update eureka env

--- src/main/java/tech/vtsign/notificationservice/NotificationServiceApplication.java
+++ src/main/java/tech/vtsign/notificationservice/NotificationServiceApplication.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableKafka
 public class NotificationServiceApplication {
     public static void main(String[] args) {
